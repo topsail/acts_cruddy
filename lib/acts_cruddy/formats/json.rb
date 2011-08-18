d73fefdd93e37e415a2977640d74ea3751285385
Mark Roghelia <mroghelia@topsailtech.com>
2011-08-18 16:12:50 -0400

    Added JSON and XML support.

--- lib/acts_cruddy/formats/json.rb
+++ lib/acts_cruddy/formats/json.rb
@@ -3,6 +3,44 @@ module ActsCruddy
   module Formats
 
     module Json
+ 
+      def index
+        @records = record_class.all
+        render :json => @records
+      end
+
+      def show
+        render :json => @record
+      end
+
+      def new
+        render :json => @record
+      end
+
+      def create
+        
+        if @record.save
+          render :json => @record, :status => :created, :location => url_for(:action => :show, :id => @record)
+        else
+          render :json => @record.errors.full_messages, :status => :unprocessable_entity
+        end
+      
+      end
+
+      def update
+          
+        if @record.update_attributes(params[@record_name])
+          head :ok
+        else
+          render :json => @record.errors.full_messages, :status => :unprocessable_entity
+        end
+        
+      end
+
+      def destroy
+        @record.destroy
+        head :ok
+      end
 
     end
 
--- lib/acts_cruddy/formats/xml.rb
+++ lib/acts_cruddy/formats/xml.rb
@@ -4,6 +4,44 @@ module ActsCruddy
 
     module Xml
 
+      def index
+        @records = record_class.all
+        render :xml => @records.to_xml(:root => plural_record_name, :dasherize => false)
+      end
+
+      def show
+        render :xml => @record.to_xml(:dasherize => false)
+      end
+
+      def new
+        render :xml => @record.to_xml(:dasherize => false)
+      end
+
+      def create
+        
+        if @record.save
+          render :xml => @record.to_xml(:dasherize => false), :status => :created, :location => url_for(:action => :show, :id => @record)
+        else
+          render :xml => @record.errors.to_xml(:dasherize => false), :status => :unprocessable_entity
+        end
+      
+      end
+
+      def update
+          
+        if @record.update_attributes(params[@record_name])
+          head :ok
+        else
+          render :xml => @record.errors.to_xml(:dasherize => false), :status => :unprocessable_entity
+        end
+        
+      end
+
+      def destroy
+        @record.destroy
+        head :ok
+      end
+
     end
 
   end
